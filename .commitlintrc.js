69a503e8dc77ed0bac4860617e1467b0f8d0c08d
Thiesset Fabien <thiessetfabienpro@gmail.com>
2025-07-02 14:06:58 +0200

    🔧 simplify commitlint configuration for better gitmoji support

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,33 +1,13 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // Type obligatoire
-    'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat', // Nouvelle fonctionnalité
-        'fix', // Correction de bug
-        'docs', // Documentation
-        'style', // Changements de style (formatting, etc.)
-        'refactor', // Refactoring
-        'perf', // Amélioration des performances
-        'test', // Ajout ou modification de tests
-        'chore', // Tâches de maintenance
-        'ci', // Changements CI/CD
-        'build', // Changements du système de build
-        'revert', // Annulation d'un commit précédent
-        'wip', // Work in progress
-        'init', // Commit initial
-      ],
-    ],
-    // Sujet obligatoire
-    'subject-empty': [2, 'never'],
+    // Configuration très permissive pour gitmoji
+    'type-empty': [0],
+    'type-enum': [0],
+    'subject-empty': [0], // Désactivé car complexe avec gitmoji
     'subject-full-stop': [2, 'never', '.'],
-    'subject-case': [2, 'always', 'lower-case'],
-    // Longueur
-    'header-max-length': [2, 'always', 72],
+    'subject-case': [0],
+    'header-max-length': [2, 'always', 100],
     'body-leading-blank': [1, 'always'],
     'body-max-line-length': [2, 'always', 100],
     'footer-leading-blank': [1, 'always'],
