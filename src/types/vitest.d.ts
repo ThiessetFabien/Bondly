bf3b482f2414a0fa4e17b35c2ec9376a798d1db3
Thiesset Fabien <thiessetfabienpro@gmail.com>
2025-08-26 11:12:30 +0200

    :green_heart: build: chang types on vitest config

--- src/types/vitest.d.ts
+++ src/types/vitest.d.ts
@@ -4,7 +4,7 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 
 declare global {
   namespace Vi {
-    interface JestAssertion<T = any>
+    interface JestAssertion<T = unknown>
       extends jest.Matchers<void>,
         TestingLibraryMatchers<string, T> {}
   }
@@ -12,10 +12,10 @@ declare global {
 
 // Augmentation des types Vitest pour Testing Library
 declare module 'vitest' {
-  interface Assertion<T = any>
+  interface Assertion<T = unknown>
     extends jest.Matchers<void>,
       TestingLibraryMatchers<string, T> {}
   interface AsymmetricMatchersContaining
     extends jest.Matchers<void>,
-      TestingLibraryMatchers<string, any> {}
+      TestingLibraryMatchers<string, unknown> {}
 }
