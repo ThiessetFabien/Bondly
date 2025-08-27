da16beba45d8f9c7da8a8982b7823126d5669a95
Thiesset Fabien <thiessetfabienpro@gmail.com>
2025-08-27 09:53:51 +0200

    :wrench: fix: remove jest on vitest config file

--- src/types/vitest.d.ts
+++ src/types/vitest.d.ts
@@ -1,21 +1,10 @@
 // Types globaux pour Vitest et Testing Library
 import '@testing-library/jest-dom'
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 
 declare global {
   namespace Vi {
-    interface JestAssertion<T = unknown>
-      extends jest.Matchers<void>,
-        TestingLibraryMatchers<string, T> {}
+    interface JestAssertion<T = unknown> {
+      // Testing Library matchers will be automatically available
+    }
   }
-}
-
-// Augmentation des types Vitest pour Testing Library
-declare module 'vitest' {
-  interface Assertion<T = unknown>
-    extends jest.Matchers<void>,
-      TestingLibraryMatchers<string, T> {}
-  interface AsymmetricMatchersContaining
-    extends jest.Matchers<void>,
-      TestingLibraryMatchers<string, unknown> {}
 }
